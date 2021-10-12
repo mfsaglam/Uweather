6d96fe26553a27f4626c24720c4dbadbbeadbc7d
mfsaglam <mfsaglam1@gmail.com>
2021-10-12 11:35:54 +0300

    Initial Commit

---
+++ Uweather/UweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  UweatherApp.swift
+//  Uweather
+//
+//  Created by Fatih Sağlam on 12.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct UweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
